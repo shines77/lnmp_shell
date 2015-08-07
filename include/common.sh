68d5d87862067000ce7f825aaa2a34e973b6fbab
shines77 <gz_shines@msn.com>
2015-08-07 21:55:04 +0800

    修正了Echo_Colors()系列函数的一个比较严重的问题.
    Signed-off-by: shines77 <gz_shines@msn.com>

--- include/common.sh
+++ include/common.sh
@@ -10,32 +10,32 @@ Color_Text()
 
 Echo_Red()
 {
-    echo $(Color_Text "$1" "31")
+    Color_Text "$1" "31"
 }
 
 Echo_Green()
 {
-    echo $(Color_Text "$1" "32")
+    Color_Text "$1" "32"
 }
 
 Echo_Yellow()
 {
-    echo $(Color_Text "$1" "33")
+    Color_Text "$1" "33"
 }
 
 Echo_Blue()
 {
-    echo $(Color_Text "$1" "34")
+    Color_Text "$1" "34"
 }
 
 Echo_Magenta()
 {
-    echo $(Color_Text "$1" "35")
+    Color_Text "$1" "35"
 }
 
 Echo_Cyan()
 {
-    echo $(Color_Text "$1" "36")
+    Color_Text "$1" "36"
 }
 
 # About shell Echo_RGBs_Ex()
@@ -47,8 +47,7 @@ Color_Text_Ex()
 
 Echo_Red_Ex()
 {
-#    echo $(Color_Text_Ex "$1" "$2" "$3" "31")
-    echo -e "$1\e[0;31m$2\e[0m$3"
+    Color_Text_Ex "$1" "$2" "$3" "31"
 }
 
 Echo_Green_Ex()
@@ -63,17 +62,17 @@ Echo_Yellow_Ex()
 
 Echo_Blue_Ex()
 {
-    echo $(Color_Text_Ex "$1" "$2" "$3" "34")
+    Color_Text_Ex "$1" "$2" "$3" "34"
 }
 
 Echo_Magenta_Ex()
 {
-    echo $(Color_Text_Ex "$1" "$2" "$3" "35")
+    Color_Text_Ex "$1" "$2" "$3" "35"
 }
 
 Echo_Cyan_Ex()
 {
-    echo $(Color_Text_Ex "$1" "$2" "$3" "36")
+    Color_Text_Ex "$1" "$2" "$3" "36"
 }
 
 # About shell Echo_RGBs_Blod()
@@ -85,32 +84,32 @@ Color_Text_Blod()
 
 Echo_Red_Blod()
 {
-    echo $(Color_Text_Blod "$1" "31")
+    Color_Text_Blod "$1" "31"
 }
 
 Echo_Green_Blod()
 {
-    echo $(Color_Text_Blod "$1" "32")
+    Color_Text_Blod "$1" "32"
 }
 
 Echo_Yellow_Blod()
 {
-    echo $(Color_Text_Blod "$1" "33")
+    Color_Text_Blod "$1" "33"
 }
 
 Echo_Blue_Blod()
 {
-    echo $(Color_Text_Blod "$1" "34")
+    Color_Text_Blod "$1" "34"
 }
 
 Echo_Magenta_Blod()
 {
-    echo $(Color_Text_Blod "$1" "35")
+    Color_Text_Blod "$1" "35"
 }
 
 Echo_Cyan_Blod()
 {
-    echo $(Color_Text_Blod "$1" "36")
+    Color_Text_Blod "$1" "36"
 }
 
 # About shell Echo_RGBs_Blod_Ex()
@@ -122,32 +121,32 @@ Color_Text_Blod_Ex()
 
 Echo_Red_Blod_Ex()
 {
-    echo $(Color_Text_Blod_Ex "$1" "$2" "$3" "31")
+    Color_Text_Blod_Ex "$1" "$2" "$3" "31"
 }
 
 Echo_Green_Blod_Ex()
 {
-    echo $(Color_Text_Blod_Ex "$1" "$2" "$3" "32")
+    Color_Text_Blod_Ex "$1" "$2" "$3" "32"
 }
 
 Echo_Yellow_Blod_Ex()
 {
-    echo $(Color_Text_Blod_Ex "$1" "$2" "$3" "33")
+    Color_Text_Blod_Ex "$1" "$2" "$3" "33"
 }
 
 Echo_Blue_Blod_Ex()
 {
-    echo $(Color_Text_Blod_Ex "$1" "$2" "$3" "34")
+    Color_Text_Blod_Ex "$1" "$2" "$3" "34"
 }
 
 Echo_Magenta_Blod_Ex()
 {
-    echo $(Color_Text_Blod_Ex "$1" "$2" "$3" "35")
+    Color_Text_Blod_Ex "$1" "$2" "$3" "35"
 }
 
 Echo_Cyan_Blod_Ex()
 {
-    echo $(Color_Text_Blod_Ex "$1" "$2" "$3" "36")
+    Color_Text_Blod_Ex "$1" "$2" "$3" "36"
 }
 
 # About shell Echo_RGBs_HalfLight()
@@ -159,32 +158,32 @@ Color_Text_HL()
 
 Echo_Red_HL()
 {
-    echo $(Color_Text_HL "$1" "31")
+    Color_Text_HL "$1" "31"
 }
 
 Echo_Green_HL()
 {
-    echo $(Color_Text_HL "$1" "32")
+    Color_Text_HL "$1" "32"
 }
 
 Echo_Yellow_HL()
 {
-    echo $(Color_Text_HL "$1" "33")
+    Color_Text_HL "$1" "33"
 }
 
 Echo_Blue_HL()
 {
-    echo $(Color_Text_HL "$1" "34")
+    Color_Text_HL "$1" "34"
 }
 
 Echo_Magenta_HL()
 {
-    echo $(Color_Text_HL "$1" "35")
+    Color_Text_HL "$1" "35"
 }
 
 Echo_Cyan_HL()
 {
-    echo $(Color_Text_HL "$1" "36")
+    Color_Text_HL "$1" "36"
 }
 
 # About shell Echo_RGBs_HalfLight_Ex()
@@ -196,32 +195,32 @@ Color_Text_HL_Ex()
 
 Echo_Red_HL_Ex()
 {
-    echo $(Color_Text_HL_Ex "$1" "$2" "$3" "31")
+    Color_Text_HL_Ex "$1" "$2" "$3" "31"
 }
 
 Echo_Green_HL_Ex()
 {
-    echo $(Color_Text_HL_Ex "$1" "$2" "$3" "32")
+    Color_Text_HL_Ex "$1" "$2" "$3" "32"
 }
 
 Echo_Yellow_HL_Ex()
 {
-    echo $(Color_Text_HL_Ex "$1" "$2" "$3" "33")
+    Color_Text_HL_Ex "$1" "$2" "$3" "33"
 }
 
 Echo_Blue_HL_Ex()
 {
-    echo $(Color_Text_HL_Ex "$1" "$2" "$3" "34")
+    Color_Text_HL_Ex "$1" "$2" "$3" "34"
 }
 
 Echo_Magenta_HL_Ex()
 {
-    echo $(Color_Text_HL_Ex "$1" "$2" "$3" "35")
+    Color_Text_HL_Ex "$1" "$2" "$3" "35"
 }
 
 Echo_Cyan_HL_Ex()
 {
-    echo $(Color_Text_HL_Ex "$1" "$2" "$3" "36")
+    Color_Text_HL_Ex "$1" "$2" "$3" "36"
 }
 
 # This is a echo color test
