1af0e26f4bc28225f7bd72393db05e17a24c18fa
shines77 <gz_shines@msn.com>
2015-08-07 16:01:23 +0800

    再次完善了 Mysql root 密码输入逻辑.
    Signed-off-by: shines77 <gz_shines@msn.com>

--- include/main.sh
+++ include/main.sh
@@ -4,8 +4,28 @@ Input_Mysql_RootPWD()
 {
     read -p "Please enter the password:   " MysqlRootPWD
     if [ "${MysqlRootPWD}" = "" ]; then
-        MysqlRootPWD="${MysqlRootDefaultPWD}"
-        MysqlRootConfirmPWD="${MysqlRootDefaultPWD}"
+        read -p "Do you sure use default Mysql password 'mysql2015'? [y/N]: " UseMysqlDefaultPWD
+
+        echo ""
+        case "${UseMysqlDefaultPWD}" in
+            [yY][eE][sS]|[yY])
+                echo "You will use default Mysql root password."
+                MysqlRootPWD="${MysqlRootDefaultPWD}"
+                MysqlRootConfirmPWD="${MysqlRootDefaultPWD}"
+            ;;
+            [nN][oO]|[nN])
+                Echo_Yellow "You have not choose the default Mysql root password, please input again."
+                Input_Mysql_RootPWD
+            ;;
+            *)
+                echo ""
+                echo "No input, You will use default Mysql root password."
+                UseMysqlDefaultPWD="y"
+                MysqlRootPWD="${MysqlRootDefaultPWD}"
+                MysqlRootConfirmPWD="${MysqlRootDefaultPWD}"
+            ;;
+        esac
+        echo ""
     else
         read -p "Please confirm the password: " MysqlRootConfirmPWD
         if [ "${MysqlRootConfirmPWD}" = "" ]; then
@@ -104,6 +124,7 @@ Dispaly_Selection()
             echo "You will install MariaDB 10.0.17."
         ;;
         *)
+            echo ""
             echo "No input, You will install MySQL 5.5.42 (Default)."
             DBSelect="2"
         ;;
@@ -139,14 +160,16 @@ Dispaly_Selection()
             echo "You will disable the InnoDB Storage Engine!"
         ;;
         *)
+            echo ""
             echo "No input, The InnoDB Storage Engine will enable."
             InstallInnodb="y"
+        ;;
     esac
     echo ""
 
 #   // set mysql root password
 
-    MysqlRootDefaultPWD="root2015"
+    MysqlRootDefaultPWD="mysql2015"
     MysqlRootPWD=""
     MysqlRootConfirmPWD=""
     Echo_Yellow "Please setup root password of MySQL. (Default password: ${MysqlRootDefaultPWD})"
